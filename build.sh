b8ea6495ae16bb417ca5e3e444f96e3091b469f1
gryber678 <gryber678@github.com>
2017-11-10 13:17:26 +0200

    upload

---
+++ build.sh
@@ -0,0 +1,1 @@
+make O=OBJ64 ARCH=arm64 CROSS_COMPILE=~///uber_aarch64-linux-android-5.3-kernel/bin/aarch64-linux-android- -j4
---
+++ init/Android.mk
@@ -0,0 +1,2 @@
+init_options += \
+    -DREBOOT_BOOTLOADER_ON_PANIC=0 \
